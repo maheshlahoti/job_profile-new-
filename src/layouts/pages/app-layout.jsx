55066bdd6478b2e81adabcc5d352c6df98f1f4cc
maheshlahoti <144707696+maheshlahoti@users.noreply.github.com>
2026-02-01 21:49:48 +0530

    Update app-layout.jsx

--- src/layouts/pages/app-layout.jsx
+++ src/layouts/pages/app-layout.jsx
@@ -12,7 +12,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-       Developed with 💗 by Mahesh
+       Developed with 💗 by Maheshcodder
       </div>
     </div>
   )
