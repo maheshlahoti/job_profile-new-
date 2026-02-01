f321d30492669394892062dc8a13a1c9110f99ec
maheshlahoti <144707696+maheshlahoti@users.noreply.github.com>
2026-02-01 21:43:49 +0530

    Update app-layout.jsx

--- src/layouts/pages/app-layout.jsx
+++ src/layouts/pages/app-layout.jsx
@@ -12,7 +12,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
+       Developed with 💗 by Mahesh
       </div>
     </div>
   )
